5bb803058771757f713aa5d14e38dcde0947c717
Kundan tiwari <112776999+glitcher007@users.noreply.github.com>
2023-10-22 00:39:55 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -25,7 +25,7 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = "bot"
+bot_name = "Sam"
 print("Let's chat! (type 'quit' to exit)")
 while True:
     # sentence = "do you use credit cards?"
@@ -45,7 +45,7 @@ while True:
 
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
-    if prob.item() > 0.5:
+    if prob.item() > 0.75:
         for intent in intents['intents']:
             if tag == intent["tag"]:
                 print(f"{bot_name}: {random.choice(intent['responses'])}")
